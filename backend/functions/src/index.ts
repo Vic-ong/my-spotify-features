027b9584e73ee2ab6e8cd88599723067c92ff9a9
Vic Ong <vickyocc53@gmail.com>
2021-06-04 11:33:13 +0800

    feat(): Remove helloworld comment

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -7,9 +7,4 @@ createModuleAlias();
 import * as functions from 'firebase-functions';
 import { createApiServer } from '@/apiServer';
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info('Hello logs!', { structuredData: true });
-//   response.send('Hello from Firebase!');
-// });
-
 exports.api = functions.https.onRequest(createApiServer());
